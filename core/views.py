870803554a3e904db82e9c5be254fd28fbc4bdee
Hasan-maqsood13 <hasanmaqsood13@gmail.com>
2025-12-04 14:20:59 +0500

    basic setup of this project

---
+++ core/views.py
@@ -0,0 +1,34 @@
+from django.shortcuts import render, redirect, HttpResponse, get_object_or_404
+from django.contrib.auth.hashers import make_password, check_password
+from django.http import JsonResponse, HttpResponseForbidden
+from django.views.decorators.cache import never_cache
+from django.views.decorators.csrf import csrf_protect
+from config.settings import EMAIL_HOST_USER
+from django.views.decorators.csrf import csrf_exempt
+from django.core.exceptions import ValidationError
+from django.core.validators import validate_email
+from django.utils.dateparse import parse_datetime
+from django.utils.timezone import make_aware
+from django.core.mail import send_mail
+from django.utils.text import slugify
+from django.core import serializers
+from django.contrib import messages
+from django.utils import timezone
+from django.conf import settings
+from urllib.parse import unquote
+from django.db.models import Sum
+from django.urls import reverse
+from datetime import timedelta
+from datetime import datetime
+import random
+import json
+import re
+
+
+def generate_verification_code(length=4):
+    """Generate a random 4-digit numeric code"""
+    return str(random.randint(1000, 9999))
+
+# Create your views here.
+def home(request):
+    return HttpResponse("Welcome to the Home Page")
